8dc5c50783246deb0a73f49df5aa652a62bcae8a
chenliupeng96 <clp9627@163.com>
2020-05-29 01:07:51 +0800

    顶部选项卡 加载更多

---
+++ fangxiaomi/components/common/card.vue
@@ -0,0 +1,39 @@
+<template>
+	<view class="card">
+		<!-- head -->
+		<view 
+		v-if="showhead"
+		class="p-1 border-bottom main-border-color"
+		>
+		<slot name="title">
+			<text calss="font-md font-weight">{{headTitle}}</text>
+		</slot>
+		
+		</view>
+		<!-- body -->
+		<view>
+			<image v-if="bodyCover" :src="bodyCover"
+			 mode="widthFix"></image>
+			 <slot name="body" ></slot>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default{
+		props:{
+			// 头部标题
+			headTitle:String,
+			// 封面图
+			bodyCover:String,
+			// 是否显示头部
+			showhead:{
+				type:Boolean,
+				default:true
+			}
+		}
+	}
+</script>
+
+<style>
+</style>
---
+++ fangxiaomi/components/index/index-nav.vue
@@ -0,0 +1,38 @@
+<template>
+	<view>
+		<view class="row j-center m-2">
+			<block 
+			v-for="(item, index) in resdata" 
+			:key="index"
+			>
+				<view 
+				class="span-4 d-flex flex-column font-sm j-center a-center py-1" 
+				@tap="event(item)" 
+				>
+					<image :src="item.src" 
+					mode="widthFix"
+					style="width: 60rpx; height: 60rpx;"
+					></image>
+					<text>{{item.text}}</text>
+				</view>
+			</block>
+			
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		props:{
+			resdata:Array
+		},
+		methods:{
+			event(item){
+				console.log(item);
+			}
+		}
+	}
+</script>
+
+<style>
+</style>
